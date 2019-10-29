17249c9dd115e52b76c475a679d6f623e86041b6
max-jellycat <max@jellycat.fr>
2019-10-29 14:47:32 +0100

    removed backend

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,28 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['react'],
   rules: {
-    'consistent-return': 0,
+    'no-confusing-arrow': 0,
+    'no-unused-expressions': 0,
     'no-unused-vars': 1,
+    'no-empty-pattern': 0,
+    'max-len': 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
   },
 };
