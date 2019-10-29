3b4c9f3415b2bb796a4558ecbe89f89e44f57eb2
max-jellycat <max@jellycat.fr>
2019-10-29 17:38:04 +0100

    redux store + success reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import successReducer from './success.reducer';
+
+export default combineReducers({
+  successReducer,
+});
---
+++ src/reducers/success.reducer.js
@@ -0,0 +1,15 @@
+/**
+ * @function successReducer
+ * @param {array} state -- Array of guessed words
+ * @param {object} action
+ * @returns {boolean} -- New success state
+ */
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
